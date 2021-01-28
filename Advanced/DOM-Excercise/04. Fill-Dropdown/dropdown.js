4bbf7b626750120d2a26b572d42fadaf501e4af8
PoweRGbg <roumen_georgiev@yahoo.com>
2021-01-28 09:39:11 +0200

    Dropdown ready

--- Advanced/DOM-Excercise/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM-Excercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    // get menu 
+    const menu = document.getElementById('menu');
+    
+    // get value of fields
+    const itemText = document.getElementById('newItemText').value;
+    const itemValue = document.getElementById('newItemValue').value;
+    // construct element option
+    const newOption = document.createElement('option');
+    newOption.textContent = itemText;
+    newOption.value = itemValue;
+    // add it to menu
+    menu.appendChild(newOption);
+
+    //clear fields
+    document.getElementById('newItemText').value = "";
+    document.getElementById('newItemValue').value = "";
 }
